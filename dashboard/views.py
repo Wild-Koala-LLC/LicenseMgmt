37c4ae61aa17baff913b67b89296c25d423cc345
wildkoala <twiggy.gordon1@gmail.com>
2020-09-14 14:00:21 -0400

    Adds /test page, working wellgit add *

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,16 @@ def index(request):
 	context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
 	return render(request, 'dashboard.html', context)
 
+def test(request):
+	soldier_list = Soldier.objects.order_by('name')
+	machine_list = Machine.objects.order_by('serial_number')
+	license_list = License.objects.order_by('-pub_date')
+	context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
+	return render(request, 'dashboard2.html', context)
+
+
+'''
 def detail(request, question_id):
 	license = get_object_or_404(License, pk=license_id)
 	return render(request, 'license.html', {'license':license})
+'''
