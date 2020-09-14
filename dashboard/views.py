ba34ee7a5ce75dfd13acc3b9c43c6807a4044b28
wildkoala <twiggy.gordon1@gmail.com>
2020-09-14 12:34:54 -0400

    Loading data from db into webpage

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import License
+from .models import License, Soldier, Machine
 
 def index(request):
+	soldier_list = Soldier.objects.order_by('name')
+	machine_list = Machine.objects.order_by('serial_number')
 	license_list = License.objects.order_by('-pub_date')
-	context = {'license_list':license_list}
+	context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
 	return render(request, 'dashboard.html', context)
 
 def detail(request, question_id):
