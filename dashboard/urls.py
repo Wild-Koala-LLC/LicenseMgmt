615305f249dca72495922cb5d0f62e8b1bb1cc40
wildkoala <twiggy.gordon1@gmail.com>
2020-10-05 15:08:16 -0400

    Add a title

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('pie_chart', views.pie_chart, name='pie_chart'),
-    path('everything', views.everything, name='everything'),
-    path('assign_licenses', views.assign_licenses, name='assign_licenses'),
     path('remaining', views.remaining, name='remaining'),
+    path('assign_licenses', views.assign_licenses, name='assign_licenses'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,33 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import License, Soldier, Machine
 
-def index(request):
-    soldier_list = Soldier.objects.order_by('name')
-    machine_list = Machine.objects.order_by('serial_number')
-    license_list = License.objects.order_by('-pub_date')
-    context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
-    return render(request, 'assigned.html', context)
-
-def assign_licenses(request):
-    soldier_list = Soldier.objects.order_by('name')
-    machine_list = Machine.objects.order_by('serial_number')
-    license_list = License.objects.order_by('-pub_date')
-    context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
-    return render(request, 'assign_licenses.html', context)
-
-def remaining(request):
-    context = {}
-    return render(request, 'remaining.html', context)
-
-def everything(request):
-    soldier_list = Soldier.objects.order_by('name')
-    machine_list = Machine.objects.order_by('serial_number')
-    license_list = License.objects.order_by('-pub_date')
-    context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
-    return render(request, 'list_everything.html', context)
 
 # User Chart.js to make a pie chart with % of a type of license used vs. Not used
-def pie_chart(request):
+def index(request):
     # total number of licenses
     total_cool_licenses = License.objects.filter(name="Cool License").count()
 
@@ -45,3 +21,20 @@ def pie_chart(request):
     data = [not_in_use, in_use]
     context = {'labels':labels, 'data':data}
     return render(request, 'pie_chart.html', context)
+
+def remaining(request):
+    soldier_list = Soldier.objects.order_by('name')
+    machine_list = Machine.objects.order_by('serial_number')
+    license_list = License.objects.order_by('-pub_date')
+    context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
+    return render(request, 'assigned.html', context)
+
+def assign_licenses(request):
+    soldier_list = Soldier.objects.order_by('name')
+    machine_list = Machine.objects.order_by('serial_number')
+    license_list = License.objects.order_by('-pub_date')
+    context = {'soldier_list':soldier_list, 'machine_list':machine_list, 'license_list':license_list}
+    return render(request, 'assign_licenses.html', context)
+
+
+
